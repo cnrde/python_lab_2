c7ba7cea8742a0441c475f102ff79e6a267d9666
cnrde <conrade.tube@gmail.com>
2023-12-08 18:20:14 +0300

    com4

--- code/main.py
+++ code/main.py
@@ -2,4 +2,14 @@ import random
 player_1 = random.randint(1,3)
 player_2 = random.randint(1, 3)
 
+if player_1 == 1: print("У первого - Камень")
+if player_1 == 2: print("У первого - Ножницы")
+if player_1 == 3: print("У первого - Бумага")
+if player_2 == 1: print("У первого - Камень")
+if player_2 == 2: print("У первого - Ножницы")
+if player_2 == 3: print("У первого - Бумага")
+
 if player_1 == player_2: print("Ничья")
+if player_1 == 1 and player_2 == 2: print("Выиграл ПЕРВЫЙ")
+if player_1 == 2 and player_2 == 3: print("Выиграл ПЕРВЫЙ")
+if player_1 == 1 and player_2 == 3: print("Выиграл ВТОРОЙ")
